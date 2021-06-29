cde1519d0d8e379cfc7ec9254df14a99d61f1edc
oluwabukola tina <oluwabukolatina@gmail.com>
2021-06-29 22:42:21 +0100

    rm unnescary files

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
 
 import morgan from 'morgan';
 /**
  * Required External Modules
  */
 import * as dotenv from 'dotenv';
-import swaggerDocument from '../swagger.json';
 import ClientRoutes from './modules/client/route/client.route';
 import AuthRoutes from './modules/auth/route/auth.route';
 import UserRoutes from './modules/user/route/user.route';
@@ -37,17 +35,9 @@ class App {
     this.invoiceRoutes.routes(this.app);
     this.app.disable('x-powered-by');
     this.app.get('/', (req, res) => res.send('Project Fibre'));
-    this.app.use(
-      '/api-docs',
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerDocument),
-    );
   }
 
   private config = (): void => {
-    // this.app.use(helmet());
-    // this.app.use(mongoSanitize());
-    // this.app.use(cors());
     this.app.use(express.json());
     this.app.use(morgan('dev'));
   };
